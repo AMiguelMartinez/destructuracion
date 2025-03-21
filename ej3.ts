aa0161c5db49cd99c92fcbab7193ad9806214464
AMiguelMartinez <158532973+AMiguelMartinez@users.noreply.github.com>
2025-03-21 16:44:48 -0500

    Delete ej3.ts

--- ej3.ts
+++
@@ -1,9 +0,0 @@
-const Usuario1 = {
-    nombre: "Juan",
-    password: "1234abc",
-}
-
-function mostrarInfo({nombre, password}: {nombre: string, password: string}): string {
-return `${nombre} tiene contraseña: ${password}`;
-}
-console.log(mostrarInfo(Usuario1));
