93a7dbb8290d1230da0252b6586124262087a5de
AMiguelMartinez <158532973+AMiguelMartinez@users.noreply.github.com>
2025-03-21 16:44:57 -0500

    Delete ej4.ts

--- ej4.ts
+++
@@ -1,6 +0,0 @@
-const nombres: string[] = ["Juan", "Pedro", "Carlos", "Marco"];
-
-function imprimirValores([primero, ...resto]: string[]): string {
-    return `valor 1: ${primero}, valor 2: ${resto}`;
-}
-console.log(imprimirValores(nombres));
